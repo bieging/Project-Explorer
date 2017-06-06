74cc47467542f70ba7228f9f7de9b101b99c5aef
Andre B <andre@selgron.com.br>
2017-06-06 16:52:23 -0300

    Mouse click function working

--- ProjectExplorer/BMath.h
+++ ProjectExplorer/BMath.h
@@ -10,11 +10,13 @@
 
 namespace bmath
 {
+	// todo - create comment for this
 	float norm(float value, float start, float stop)
 	{
 		return (value - start) / (stop - start);
 	}
 
+	// todo - create comment for this
 	float map(float value,
 		float start1, float stop1,
 		float start2, float stop2)
---
+++ ProjectExplorer/UI.h
@@ -0,0 +1,13 @@
+#ifndef UI_H
+#define UI_H
+
+#pragma once
+
+class UI
+{
+public:
+	UI();
+	~UI();
+};
+
+#endif // UI_H
